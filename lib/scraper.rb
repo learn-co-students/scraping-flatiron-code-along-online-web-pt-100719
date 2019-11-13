7c3048b4fd212c213176af9d43c07dad0febb9cb
laffitte.digital@gmail.com <laffitte.digital@gmail.com>
2019-11-12 20:37:28 -0500

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -4,7 +4,25 @@ require 'open-uri'
 require_relative './course.rb'
 
 class Scraper
-  
+
+  def get_page
+    html = open("http://learn-co-curriculum.github.io/site-for-scraping/courses")
+    Nokogiri::HTML(html)
+  end
+
+  def get_courses
+    self.get_page.css(".posts-holder .post")
+  end
+
+  def make_courses
+    self.get_courses.map do |course| 
+      new_course = Course.new
+      new_course.title = course.css("h2").text
+      new_course.schedule = course.css(".date").text
+      new_course.description = course.css("p").text
+    end
+  end
+
   def print_courses
     self.make_courses
     Course.all.each do |course|
@@ -18,5 +36,6 @@ class Scraper
   
 end
 
-
+my_scrape = Scraper.new
+puts my_scrape.get_courses
 
