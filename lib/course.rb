4199a0017d90ab4bd9b7a643358af60876aff000
github username <github email address>
2019-11-28 13:17:56 -0500

    Done.

--- lib/course.rb
+++ lib/course.rb
@@ -1,23 +1,19 @@
 class Course
  
     attr_accessor :title, :schedule, :description
-    
+   
     @@all = []
-
-    def intialize
-        @@all << self
+   
+    def initialize
+      @@all << self
     end
-    
+   
     def self.all
-        @@all
+      @@all
     end
-    
+   
     def self.reset_all
-        @@all.clear
-    end    
-
-end
-
-
-
-
+      @@all.clear
+    end
+   
+  end
